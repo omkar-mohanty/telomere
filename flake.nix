063de1974bff001475a87ce65f7404f09593ff1a
omkar-mohanty <franzohouser@gmail.com>
2026-05-16 21:08:23 +0530

    add rust-overlay

--- flake.nix
+++ flake.nix
@@ -1,6 +1,7 @@
 {
   inputs = {
     flake-utils.url = "github:numtide/flake-utils";
+    rust-overlay.url = "github:oxalica/rust-overlay";
     naersk = {
       url = "github:nix-community/naersk";
       inputs.nixpkgs.follows = "nixpkgs";
@@ -8,11 +9,12 @@
     nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
   };
 
-  outputs = { flake-utils, naersk, nixpkgs, ... }:
+  outputs = { flake-utils, naersk, nixpkgs, rust-overlay,... }:
     flake-utils.lib.eachDefaultSystem (system:
       let
+      overlays = [ (import rust-overlay) ];
         pkgs = (import nixpkgs) {
-          inherit system;
+          inherit system overlays;
         };
 
 
@@ -22,12 +24,12 @@
         # For `nix build` & `nix run`:
         packages.default = naersk'.buildPackage {
           src = ./.;
+          nativeBuildInputs = with pkgs; [ rust-bin.beta.latest.default ];
         };
 
         # For `nix develop`:
         devShell = pkgs.mkShell {
-          nativeBuildInputs = with pkgs; [rust-analyzer rustc cargo ];
-
+          nativeBuildInputs = with pkgs; [ rust-bin.beta.latest.default ];
         };
       }
     );
