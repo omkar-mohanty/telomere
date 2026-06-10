4fcc0cfc704d8c7470290b31735069f1b9d43c7f
omkar-mohanty <franzohouser@gmail.com>
2026-06-10 14:14:22 +0530

    upload downloader

--- telomere-core/src/downloader.rs
+++ telomere-core/src/downloader.rs
@@ -1,56 +1,23 @@
 use anyhow::{Error, Result};
 
-use grammers_client::media::Media;
+use grammers_client::media::{Downloadable, Media};
 use grammers_client::sender::RpcError;
 use grammers_client::{Client, InvocationError};
-use grammers_session::types::PeerRef;
 use std::path::PathBuf;
 use std::sync::Arc;
 use tokio::fs::OpenOptions;
 use tokio::io::AsyncWriteExt;
+use tokio::sync::mpsc::unbounded_channel;
 use tokio::sync::{
     Semaphore,
     mpsc::{UnboundedReceiver, UnboundedSender},
 };
 use tokio::task::JoinSet;
 
-pub struct DownlaoderBuilder {
-    limit: Option<usize>,
-    client: Client,
-    dst_root: Option<PathBuf>,
-}
-
-impl DownlaoderBuilder {
-    pub fn new(client: Client, peer: PeerRef) -> Self {
-        DownlaoderBuilder {
-            limit: None,
-            dst_root: None,
-            client,
-        }
-    }
-
-    pub fn set_limit(mut self, limit: usize) -> Self {
-        self.limit = Some(limit);
-        self
-    }
-
-    pub fn set_dst(mut self, path: PathBuf) -> Self {
-        self.dst_root = Some(path);
-        self
-    }
-    pub fn build(self) -> Result<Downloader> {
-        Ok(Downloader {
-            client: self.client,
-            semaphore: Arc::new(Semaphore::new(self.limit.unwrap_or(1))),
-            tasks: JoinSet::new(),
-        })
-    }
-}
-
 pub struct DownloadTask {
-    medias: Vec<Media>,
-    download_folder: PathBuf,
+    media: Media,
     retries: Option<usize>,
+    filename: PathBuf,
 }
 
 pub struct Downloader {
@@ -59,37 +26,63 @@ pub struct Downloader {
     tasks: JoinSet<Result<()>>,
 }
 
+impl Downloader {
+    pub fn new(client: Client, limit: usize) -> Self {
+        Self {
+            client,
+            semaphore: Arc::new(Semaphore::new(limit)),
+            tasks: JoinSet::new(),
+        }
+    }
+
+    pub async fn enqueue_task(&mut self, task: DownloadTask) -> UnboundedReceiver<DownloadEvent> {
+        let (tx, rx) = unbounded_channel();
+        let client = self.client.clone();
+        let permit = self.semaphore.clone();
+        self.tasks.spawn(async move {
+            let _permit = permit.acquire().await?;
+            task.download_media(client, tx).await?;
+            Ok::<(), Error>(())
+        });
+        rx
+    }
+
+    pub async fn finish_all_tasks(&mut self) -> Vec<Result<()>> {
+        let tasks = std::mem::take(&mut self.tasks);
+        tasks.join_all().await
+    }
+}
+
 impl DownloadTask {
     async fn download_media(
         self,
         client: Client,
-        media: Media,
         tx: UnboundedSender<DownloadEvent>,
-        folder_path: PathBuf,
     ) -> Result<()> {
         use InvocationError::*;
-        todo!("Implement getting filename from media");
-        let path = folder_path.join("/filename.file");
-        match media {
-            Media::Photo(p) => {}
-            Media::Document(doc) => {}
-            _ => {
-                return Err(anyhow::Error::msg("Unsupported Media Type"));
-            }
-        };
-        let mut stream = client.iter_download(&media);
+        let total_size = self.media.size().unwrap_or(100);
+        let mut total_retries = 0;
+
+        let mut stream = client.iter_download(&self.media);
+
         let mut file = OpenOptions::new()
             .create_new(true)
             .write(true)
             .truncate(true)
-            .open(&path)
+            .open(&self.filename)
             .await?;
+        let mut total_downloaded = 0;
 
         loop {
             match stream.next().await {
                 Ok(Some(chunk)) => {
                     file.write_all(&chunk).await?;
                     file.flush().await?;
+                    total_downloaded += chunk.len();
+                    tx.send(DownloadEvent::Progress {
+                        total_size,
+                        total_downloaded,
+                    })?;
                 }
                 Ok(None) => return Ok(()),
                 Err(e) => match &e {
@@ -103,14 +96,25 @@ impl DownloadTask {
                             code: 400, value, ..
                         } => {
                             log::error!("RPC Error File Ref Expired : {:?}", value);
+                            if let Some(retries) = self.retries
+                                && total_retries < retries
+                            {
+                                total_retries += 1;
+                                log::info!(
+                                    "Retrying Download for : {:?} Attempt : {}",
+                                    self.filename,
+                                    total_retries
+                                );
+                                stream = client.iter_download(&self.media);
+                            }
                         }
                         _ => {
-                            let _ = tx.send(e.into());
+                            tx.send(e.into())?;
                             return Ok(());
                         }
                     },
                     _ => {
-                        let _ = tx.send(e.into());
+                        tx.send(e.into())?;
                         return Ok(());
                     }
                 },
@@ -118,8 +122,6 @@ impl DownloadTask {
         }
     }
 }
-
-impl Downloader {}
 
 impl<E: std::error::Error + Send + Sync + 'static> From<E> for DownloadEvent {
     fn from(value: E) -> Self {
@@ -130,9 +132,8 @@ impl<E: std::error::Error + Send + Sync + 'static> From<E> for DownloadEvent {
 pub enum DownloadEvent {
     Finished,
     Progress {
-        filename: String,
-        total_size: u64,
-        total_downloaded: u64,
+        total_size: usize,
+        total_downloaded: usize,
     },
     Error(Error),
 }
